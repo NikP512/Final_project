a6908b2fa38cc8fcee854bf24bd86a323adac251
AlChernyshev <alex20052009234@mail.ru>
2023-12-09 17:35:23 +0300

    Улучшил движение игрока

--- platformer_layer.py
+++ platformer_layer.py
@@ -32,7 +32,7 @@ class ScreenLayer:
                     self.objects.append(Wall(self.screen, int(x), int(y), int(w), int(h)))
 
 
-def check_space(main_object, objects):
+def check_space(main_object, objects, screen, hitbox_on):
     """функция для проверки есть ли свободное место с каждой стороны от объекта,
      только для объектов с прямоугольным хитбоксом
     main_object -- объект для которого провереяется наличее свободного места,
@@ -40,16 +40,19 @@ def check_space(main_object, objects):
     возвращает список булевых значений описывающие есть ли
     с соответствующей стороны от игрока свободное место True -- есть, False -- нет
     порядок в списке: вверх, право, низ, лево
+    hitbox_on -- булева переменая которая определяет отрисовывается ли хитбокс
      """
-    rect_up = pygame.Rect(main_object.x - main_object.w / 3, main_object.y - main_object.h / 2 - 1, main_object.w/1.5, 2)
-    rect_right = pygame.Rect(main_object.x + main_object.w / 2 + 1, main_object.y - main_object.h / 2.4, 2, main_object.h/1.2)
-    rect_down = pygame.Rect(main_object.x - main_object.w / 3, main_object.y + main_object.h / 2 + 1, main_object.w/1.5, 2)
-    rect_left = pygame.Rect(main_object.x - main_object.w / 2 - 1, main_object.y - main_object.h / 2.4, 2, main_object.h/1.2)
+    rect_up = pygame.Rect(main_object.x - main_object.w / 2.4, main_object.y - main_object.h / 2 - 1, main_object.w/1.2, 2)
+    rect_right = pygame.Rect(main_object.x + main_object.w / 2 , main_object.y - main_object.h / 2.1, 2, main_object.h/1.05)
+    rect_down = pygame.Rect(main_object.x - main_object.w / 2.4, main_object.y + main_object.h / 2 + 1, main_object.w/1.2, 2)
+    rect_left = pygame.Rect(main_object.x - main_object.w / 2 - 2, main_object.y - main_object.h / 2.1, 2, main_object.h/1.05)
     free_spaces = [False, False, False, False]
 
     for object in objects:
         rect_object = pygame.Rect(object.x - object.w / 2, object.y - object.h / 2, object.w, object.h)
         for rect, n in zip([rect_up, rect_right, rect_down, rect_left], range(0, 4)):
+            if hitbox_on:
+                pygame.draw.rect(screen, (255, 0, 0), rect)
             free_spaces[n] += rect.colliderect(rect_object)
     for n in range(0, 4):
         free_spaces[n] = not free_spaces[n]
--- platformer_main.py
+++ platformer_main.py
@@ -42,18 +42,20 @@ def main():
     player = Player(screen, layers[0].id)
     player.get_coordinates(300, 600)
     layers[0].objects.append(Wall(screen, 400, 700, 800, 100))
+    layers[0].objects.append(Wall(screen, 100, 700, 50, 1000))
+    layers[0].objects.append(Wall(screen, 500, 700, 50, 1000))
     layers[0].set_object_from_file("1.1")
     info = [True, True, True, True]
 
     while RUNNING:
         screen.fill((255, 255, 255))
         keys = check_events()
+        update_player(player, keys, info)
         for layer in layers:
             if layer.id == player.layer_id:
-                info = check_space(player, layer.objects)
+                info = check_space(player, layer.objects, screen, False)
                 layer.update()
                 break
-        update_player(player, keys, info)
         pygame.display.update()
 
         clock.tick(FPS)
