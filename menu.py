aea92b06b0c438913a3d1b604d1b5c15560b8f75
Nikita Payusov <npayusov1@gmail.com>
2023-12-18 20:36:49 +0300

    Добавлены задания в menu.py для Kolya123F.

--- menu.py
+++ menu.py
@@ -7,20 +7,25 @@ class Menu:
         self.running = True
         self.choose_level = 0
         self.choose_player = 0
+        self.background_image = None
+        self.button_image = None
         self.buttons = []
 
     def update(self):
-        self.screen.fill((255, 255, 255))
+        """Добавить отрисовку заднего фона и кнопок по аналогии с objects.py"""
+        pass
 
     def check_events(self, events):
         for event in events:
             if event.type == pygame.QUIT:
                 self.running = False
                 break
+            # Добавить проверку, что мы ткнули на кнопку. Использовать pygame.collidepoint
             pass
 
 
 class Button:
+    """Добавить констркутор. Атрибуты: координатцы центра кнопки x, y и размеры w, h."""
     pass
 
 
